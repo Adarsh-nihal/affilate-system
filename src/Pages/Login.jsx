50c0e4e2c9ef0a841ceacc1aa4300239cec944ce
Adarsh-nihal <adarsh474747@gmail.com>
2024-09-08 22:13:11 +0530

    added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -30,26 +30,27 @@ const toast=useToast()
     <div className='flex w-full  md:h-[100vh] md:items-center md:justify-center p-0 overflow-hidden'>
       <form
         onSubmit={handleSubmit}
-        className="bg-white dark:bg-zinc-900 md:h-[80%] md:border md:border-[#FDB743] w-full p-2 md:w-[500px] h-full  md:rounded-2xl"
+        className="bg-zinc-900  md:h-[80%] md:border md:border-[#FDB743] w-full p-2 md:w-[500px] h-[100vh]  md:rounded-2xl"
       >
         <div className="px-8 py-10 md:px-10">
-          <h2 className="text-4xl font-extrabold text-center text-zinc-800 dark:text-white">
+          <h2 className="text-4xl font-extrabold text-center text-whi-800 ">
             Welcome Back!
           </h2>
-          <p className="text-center text-zinc-600 dark:text-zinc-400 mt-3">
+          <p className="text-center text-zinc-600  mt-3">
             We missed you, sign in to continue.
           </p>
           <div className="mt-10">
             <div className="relative">
               <label
-                className="block mb-3 text-sm font-medium text-zinc-600 dark:text-zinc-200"
+                className="block mb-3 text-sm font-medium text-zinc-600 "
                 htmlFor="email"
               >
                 Email
               </label>
               <input
                 placeholder="adarsh@example.com"
-                className="block w-full outline-none px-4 py-3 mt-2 text-zinc-800 bg-white border-2 rounded-lg dark:border-zinc-600 dark:bg-zinc-800 dark:text-zinc-200 focus:border-yellow-500  "
+                className="block w-full outline-none px-4 py-3 mt-2 text-zinc-800 bg-[#27272A] border-2 rounded-lg border-gray-700 focus:border-yellow-500  "
+
 
                 name="email"
                 id="email"
@@ -60,14 +61,14 @@ const toast=useToast()
             </div>
             <div className="mt-6">
               <label
-                className="block mb-3 text-sm font-medium text-zinc-600 dark:text-zinc-200"
+                className="block mb-3 text-sm font-medium text-zinc-600 "
                 htmlFor="password"
               >
                 Password
               </label>
               <input
                 placeholder="••••••••"
-                className="block w-full outline-none px-4 py-3 mt-2 text-zinc-800 bg-white border-2 rounded-lg dark:border-zinc-600 dark:bg-zinc-800 dark:text-zinc-200 focus:border-yellow-500  "
+                className="block w-full outline-none px-4 py-3 mt-2 text-zinc-800 bg-[#27272A] border-2 rounded-lg border-gray-700 focus:border-yellow-500  "
                 name="password"
                 id="password"
                 type="password"
@@ -85,7 +86,7 @@ const toast=useToast()
             </div>
           </div>
         </div>
-        <div className="px-8 py-4 bg-blue-200 dark:bg-zinc-800">
+        <div className="px-8 py-4 rounded-md bg-zinc-800">
           <div className="text-sm text-[#FDB743] text-center">
             forgot your password ? Please connect with admin !
             {/* <a className="font-medium underline" href="#">
